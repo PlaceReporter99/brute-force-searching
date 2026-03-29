5f3a7c480e9ebd508ff68ae4e1f21b0778ffe19e
Little Monkey <106759534+PlaceReporter99@users.noreply.github.com>
2026-03-29 12:12:00 +0100

    Refactor find-plustimes-sol.py for clarity and efficiency

--- find-plustimes-sol.py
+++ find-plustimes-sol.py
@@ -1,52 +1,69 @@
 # https://codegolf.stackexchange.com/questions/287678/generate-a-large-number-using-and-%C3%97
 # Input to this program is made as arguments, where the first argument indicates what program length to start from, and 
-import sys
-def generate_all_poss(instructions: list, step_count: int): # instructions in format [({variable, variable}, operator),...]. Variable is int representing variable number, operator is 0 for addition and 1 for multiplication. For example, x3 * x4 is ({3, 4}, 1).
-	if len(instructions) >= step_count:
-		return instructions
-	elif len(instructions) == 0:
-		return generate_all_poss([({0}, 0)], step_count)
-	else:
-		return[generate_all_poss(instructions + [({f, s}, o)], step_count)for f in range(len(instructions) + 1)for s in range(len(instructions) + 1)for o in[0,1]]
 
-leng = 12
+import math
+
+def contains_duplicate(l):
+	dup = set()
+	for i in l:
+		if str(i) in dup:
+			return True
+		else:
+			dup.add(str(i))
+	return False
+
+def numtoradi(num, length):
+	a = []
+	for i in range(1, length+1):
+		a.append(num%i)
+		num = num // i
+	return a
+
+def numtobin(num, length):
+	return [*map(int, bin(num)[2:].zfill(length))]
+
+def generate_all_poss(step_count: int): # instructions in format [({variable, variable}, operator),...]. Variable is int representing variable number, operator is 0 for addition and 1 for multiplication. For example, x3 * x4 is ({3, 4}, 1).
+	for a in range(math.factorial(step_count)):
+		for b in range(math.factorial(step_count)):
+			for c in range(2**step_count):
+				yield [({i,j}, k) for i,j,k in zip(numtoradi(a, step_count), numtoradi(b, step_count), numtobin(c, step_count))]
+				
+
+leng = 1
 find = 10407932194664399081925240327364085538615262247266704805319112350403608059673360298012239441732324184842421613954281007791383566248323464908139906605677320762924129509389220345773183349661583550472959420547689811211693677147548478866962501384438260291732348885311160828538416585028255604666224831890918801847068222203140521026698435488732958028878050869736186900714720710555703168729087
 
 def get_values(length):
-	a = [*filter(lambda x:not ({0}, 1) in x, generate_all_poss([], length))]
-	try:
-		while type(a[0][0]) is list:
-			b = []
-			for i in a:
-				b += [*i]
-			a = b
-	except:
-		pass
-
-	values = []
-	if len(a) == 1:
-		a = [a]
+	a = filter(lambda x:not contains_duplicate(x) and not ({0}, 1) in x, generate_all_poss(length))
+	used = set()
 	for i in a:
-		s = [1]
-		for j in i:
-			if len(j[0]) == 1:
-				aa = j[0].pop()
-				j[0].add(aa)
-				s.append(eval(f"{s[aa]} {['+', '*'][j[1]]} {s[aa]}"))
-			else:
-				bb = j[0].pop()
-				cc = j[0].pop()
-				j[0].add(bb)
-				j[0].add(cc)
-				s.append(eval(f"{s[bb]} {['+', '*'][j[1]]} {s[cc]}"))
-		values.append(s[-1])
-	return zip(a, values)
+		if str(i) not in used:
+			used.add(str(i))
+			s = [1]
+			for j in i:
+				if len(j[0]) == 1:
+					aa = j[0].pop()
+					j[0].add(aa)
+					if j[1]:
+						s.append(s[aa] * s[aa])
+					else:
+						s.append(s[aa] + s[aa])
+				else:
+					bb = j[0].pop()
+					cc = j[0].pop()
+					j[0].add(bb)
+					j[0].add(cc)
+					if j[1]:
+						s.append(s[bb] * s[cc])
+					else:
+						s.append(s[bb] + s[cc])
+			yield i, s[-1]
 
 s = False
 while not s:
 	for i,j in get_values(leng):
+		#print(str(i)+" "*(15*leng - len(str(i)))+str(j))
 		if j == find:
-			print(str(i)+" "*(15*leng - len(str(i)))+str(j))
+			print("solution found for length",leng,"yielding",find,"which is above")
 			s = True
 			break
 	if not s:
